0e7d79c8178c1a0c00dd6c802f4b93b9b8be2d43
PuckyIT <phat30102002@gmail.com>
2024-11-04 05:22:45 +0700

    fix loading page

--- src/pages/login/callback.tsx
+++ src/pages/login/callback.tsx
@@ -1,30 +1,48 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useTheme } from "@/untils/ThemeContext";
+import Image from "next/image";
 
 const LoginCallback = () => {
   const router = useRouter();
+  const { theme } = useTheme(); // Get the current theme
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const token = urlParams.get("token");
     const email = urlParams.get("email");
-    const avatar = urlParams.get("avatar"); // Lấy avatar từ URL
+    const avatar = urlParams.get("avatar"); // Get avatar from URL
 
     if (token) {
-      // Lưu token, email, và avatar vào localStorage
+      // Save token, email, and avatar to localStorage
       localStorage.setItem("token", token);
-      localStorage.setItem("user", JSON.stringify({ email, avatar })); // Lưu avatar cùng với email
+      localStorage.setItem("user", JSON.stringify({ email, avatar })); // Save avatar along with email
 
-      // Điều hướng đến trang home hoặc trang người dùng
+      // Navigate to home or user page
       router.push("/home");
     } else {
-      // Điều hướng đến trang đăng nhập nếu không có token
+      // Navigate to login page if no token
       router.push("/login");
     }
   }, [router]);
 
-  return <p>Đang xử lý đăng nhập...</p>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        backgroundColor: theme === "dark" ? "#121212" : "#ffffff",
+        color: theme === "dark" ? "#ffffff" : "#000000", 
+        flexDirection: "column",
+      }}
+    >
+      <Image src="/logo.png" alt="Logo" width={212} height={212} />
+      <p>Đang xử lý đăng nhập...</p>
+    </div>
+  );
 };
 
 export default LoginCallback;
